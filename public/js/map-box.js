0e10373074567d2d080f0e0352475a986c5d7571
Farman <farmancs2024@gmail.com>
2025-03-03 02:08:25 +0500

    adding mapboxgl to the porject

--- public/js/map-box.js
+++ public/js/map-box.js
@@ -1,17 +1,3 @@
-// console.log("wellcome to clint side");
-// const locations = JSON.parse(document.getElementById('map').dataset.locations);
-// console.log(locations);
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoiZmFybWFuY3MyMDI0IiwiYSI6ImNtN2x5N3BsNzBoNzcyaXNkaGg1cTV3aTkifQ.ZkdHSHSNrpFR0Ii3RV6aQg';
-// const map = new mapboxgl.Map({
-//    container: 'map',
-//    style: 'mapbox://styles/farmancs2024/cm7ls402a00c901sb05h03kn7',
-//    center: [-112.987418, 37.198125],
-//    zoom: 9 // starting zoom
-// });
-
-
-// const mapboxgl = require('mapbox-gl')
 console.log("Welcome to client side");
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
@@ -19,19 +5,44 @@ console.log(locations);
 mapboxgl.accessToken =
    'pk.eyJ1IjoiZmFybWFuY3MyMDI0IiwiYSI6ImNtN2x5N3BsNzBoNzcyaXNkaGg1cTV3aTkifQ.ZkdHSHSNrpFR0Ii3RV6aQg';
 
-//pk.eyJ1IjoiZmFybWFuY3MyMDI0IiwiYSI6ImNtN2x5N3BsNzBoNzcyaXNkaGg1cTV3aTkifQ.ZkdHSHSNrpFR0Ii3RV6aQg
 const map = new mapboxgl.Map({
    container: 'map', // container ID
    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-   center: [-74.5, 40], // starting position [lng, lat]
-   zoom: 9, // starting zoom
+   scrollZoom: false
 });
 
-// mapboxgl.accessToken = 'pk.eyJ1IjoiZmFybWFuY3MyMDI0IiwiYSI6ImNtN2x5N3BsNzBoNzcyaXNkaGg1cTV3aTkifQ.ZkdHSHSNrpFR0Ii3RV6aQg';
+const bounds = new mapboxgl.LngLatBounds()
 
-// const map = new mapboxgl.Map({
-//    container: 'map',
-//    style: 'mapbox://styles/farmancs2024/cm7ls402a00c901sb05h03kn7',
-//    center: [-112.987418, 37.198125],
-//    zoom: 9
-// });
+locations.forEach(loc => {
+   // creating marker to locastion
+   let el = document.createElement('div');
+   el.className = 'marker';
+
+   //add marker 
+   new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+   })
+      .setLngLat(loc.coordinates)
+      .addTo(map)
+
+   //add popup message
+   new mapboxgl.Popup({
+      offset: 20
+   })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</P>`)
+      .addTo(map)
+
+   //extend the coordinates
+   bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+   padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+   }
+})
